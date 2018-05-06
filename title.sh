fe2a0b789a22558072bea3fbfb82bf0cf700a44d
spartrekus <spartrekus@gmail.com>
2018-05-06 14:43:22 +0200

    Add files via upload

---
+++ title.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+cat EPUB/book.opf  | grep "dc:title"
+
+exit
+if [ $# -lt 3 ]
+then
+   echo
+   echo "Usage: minfo -m <meta-type> <epub-file>"
+   echo
+else
+   fileloc=`unzip -l "$3" | grep -Po '\b[^\s-]*\.opf\b'`
+   metafound=`zipgrep  '<dc:'$2'>(.*)</dc:'$2'>' "$3" $fileloc`
+   echo `expr "$metafound" : '.*<dc:'$2'>\(.*\)</dc:'$2'>.*'`
+fi
